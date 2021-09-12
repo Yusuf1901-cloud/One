59cc3c28366b1079ea455b9c8ede426826b441a8
Yusuf1901-cloud <82632061+Yusuf1901-cloud@users.noreply.github.com>
2021-09-12 08:16:24 +0500

    Update Fibonacci.go

--- Fibonacci.go
+++ Fibonacci.go
@@ -1,1 +1,25 @@
+package main
 
+import "fmt"
+
+
+func main() {
+    var n int
+    fmt.Print("Enter the number of items: ")
+    fmt.Scanln(&n)
+    fib1 := 1
+    fib2 := 1
+    
+
+    fmt.Println(fib1)
+    fmt.Println(fib2)
+    
+    for i:=0; i<n; i++{
+        fib3 := fib1 + fib2
+        fmt.Println(fib3)
+        fib1 = fib2
+        fib2 = fib3
+        
+        
+    }  
+}
