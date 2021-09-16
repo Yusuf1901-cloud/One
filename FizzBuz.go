acf421114ac8b8b36b0bc2186f2001bb248e5cda
Yusuf1901-cloud <82632061+Yusuf1901-cloud@users.noreply.github.com>
2021-09-16 22:03:37 +0500

    Update FizzBuz.go

--- FizzBuz.go
+++ FizzBuz.go
@@ -1,1 +1,23 @@
+package main
 
+import "fmt"
+
+func main(){
+	var n int
+	fmt.Scanln(&n)
+  fmt.Println("These are Fizzbuz numbers !!!")
+
+	for i:=1; i<=n; i++ {
+		if (i % 3 != 0){
+			if (i % 5 != 0){
+				fmt.Println(i)
+			} else {
+				fmt.Println("Buzz")
+			  }
+		} else if ( i % 5 != 0){
+			fmt.Println("Fizz")
+			   }else {
+				    fmt.Println("FizzBuz") 
+			    }
+	}
+}
