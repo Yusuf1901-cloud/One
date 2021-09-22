83b291da467bdfc6c0b1078ef613f8592c754b76
Yusuf Tilavov <82632061+Yusuf1901-cloud@users.noreply.github.com>
2021-09-22 20:55:06 +0500

    Update duplicate.go

--- duplicate.go
+++ duplicate.go
@@ -2,28 +2,18 @@ package main
 
 import "fmt"
 
-func main(){
-	var n int 
-	arr := make([]int, 0)
-	fmt.Print("Enter number your arr items: ")
-	fmt.Scanln(&n)
-	fmt.Println("Now enter elements of your array : ")
-	for i:=0; i<n; i++{
-		var n0 int
-		fmt.Scanln(&n0)
-		arr = append(arr, n0)
+func fibonacci() func() int {
+	fib1, fib2 := 1,1
+	return func() int{
+		fib := fib1
+		fib1, fib2 = fib2, fib1 + fib2
+		return fib
 	}
-	fmt.Print("Your array: ")
-	fmt.Println(arr)
-	fmt.Println(len(arr))
-	for i:=0; i<len(arr); i++{
-		for j:=i+1; j< len((arr)); j++{
-			if arr[j] == arr[i]{
-				fmt.Printf("In your arr has duplicate: %v\n", arr[i])
+}
 
-			}
-		}
-
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
 	}
-	
 }
